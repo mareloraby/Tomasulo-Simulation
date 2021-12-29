5e18bc88988482c01136757e9ff56ac89aef6d12
Maryam <maryam.eloraby@gmail.com>
2021-12-29 17:22:44 +0200

    Added inputs

--- Backend.js
+++ Backend.js
@@ -1,5 +1,5 @@
-var memory = [];
-var executionTimes = [];
+var executionTimes;
+
 var instructionsQ = [];
 var AddReserv = [];
 var MulReserv = [];
@@ -37,24 +37,75 @@ var Registers = [
   { name: "F28", Q: "", V: 0 },
   { name: "F29", Q: "", V: 0 },
   { name: "F30", Q: "", V: 0 },
-  { name: "F31", Q: "", V: 0 }
-
+  { name: "F31", Q: "", V: 0 },
 ];
+var memory = [];
+
+function start() {
+  document.getElementById("startBtn").style.visibility = "hidden";
+  document.getElementById("nextBtn").style.visibility = "visible";
+
+  executionTimes = {
+    LDet: document.getElementsByName("LD")[0].value,
+    SDet: document.getElementsByName("SD")[0].value,
+    ADDSUBet: document.getElementsByName("ADDSUB")[0].value,
+    MULet: document.getElementsByName("MUL")[0].value,
+    DIVet: document.getElementsByName("DIV")[0].value,
+  };
 
 
+  //Save instruction table
+  for (var i = 1; i < document.getElementById("IQ").rows.length; i++) {
+    if (
+      document
+        .getElementById("IQ")
+        .rows[i].cells[0].getElementsByTagName("select")[0].value != "OP"
+    ) {
+      var newrow = {
+        OP: document
+          .getElementById("IQ")
+          .rows[i].cells[0].getElementsByTagName("select")[0].value,
+        dest: ""+  document
+        .getElementById("IQ")
+        .rows[i].cells[1].innerHTML,
+        R1: "",
+        R2: "",
+        Issue: 0,
+        Exec: 0,
+        writeRes: 0,
+      };
 
+      instructionsQ = instructionsQ.push(newrow);
+    }
+
+    document
+    .getElementById("IQ")
+    .rows[i].cells[0].getElementsByTagName("select")[0].disabled=true;
+
+    document
+    .getElementById("IQ")
+    .rows[i].cells[1].getElementsByTagName("input")[0].disabled=true;
+  }
+
+  document.getElementsByName("LD")[0].disabled = true;
+  document.getElementsByName("SD")[0].disabled = true;
+  document.getElementsByName("ADDSUB")[0].disabled = true;
+  document.getElementsByName("MUL")[0].disabled = true;
+  document.getElementsByName("DIV")[0].disabled = true;
+
+  console.log(executionTimes.SDet);
+}
+
+function next() {
+  incrementClk();
+}
 
 var clkCycle;
-function incrementClk(){
-    clkCycle = parseInt(document.getElementById('getClkCycle').innerHTML, 10);
-    clkCycle = isNaN(clkCycle) ? 0 : clkCycle;
-    clkCycle++;
-    document.getElementById('getClkCycle').innerHTML = clkCycle;
-
-
-
-
-    
+function incrementClk() {
+  clkCycle = parseInt(document.getElementById("getClkCycle").innerHTML, 10);
+  clkCycle = isNaN(clkCycle) ? 0 : clkCycle;
+  clkCycle++;
+  document.getElementById("getClkCycle").innerHTML = clkCycle;
 }
 
 // var myTable = document.getElementById('IQ');
