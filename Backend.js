4d5e154d6d0989657ff717a951460da7ca55d672
Maryam <maryam.eloraby@gmail.com>
2021-12-31 22:26:31 +0200

    DIV done

--- Backend.js
+++ Backend.js
@@ -301,7 +301,19 @@ function execute() {
           }
           break;
         case "DIV":
-          return MulReservAvailable();
+          if (
+            AddReserv[index].Vj != "" &&
+            AddReserv[index].Vk != "" &&
+            instructionsQ[i].exec == 0
+          ) {
+            instructionsQ[i].exec = clkCycle;
+
+            instructionsQ[i].time =
+              parseInt(clkCycle) + parseInt(executionTimes.DIVet) - 1;
+            instructionsQ[i].result =
+              parseInt(AddReserv[index].Vj + "", 10) / parseInt(AddReserv[index].Vk + "", 10);
+          }
+          break;
       }
     }
   }
